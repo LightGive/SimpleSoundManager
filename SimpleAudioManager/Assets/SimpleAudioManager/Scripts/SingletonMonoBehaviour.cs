94f779fc08e3754c50456708ab0dbd9bc95c9404
VIVE_PC_3 <roi.akase@outlook.jp>
2017-10-02 17:01:02 +0900

    シングルトンを修正

--- SimpleAudioManager/Assets/SimpleAudioManager/Scripts/SingletonMonoBehaviour.cs
+++ SimpleAudioManager/Assets/SimpleAudioManager/Scripts/SingletonMonoBehaviour.cs
@@ -1,65 +1,32 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-/// <summary>
-/// シングルトンクラス
-/// GameControllerタグが付いているオブジェクトを参照する
-/// </summary>
-/// <typeparam name="T"></typeparam>
-public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
 {
-	/// <summary>
-	/// タグの名前
-	/// </summary>
-	protected static readonly string MANAGER_TAG = "GameController";
-	
-	//インスタンス
-	protected static T instance;
+	private static T instance;
 	public static T Instance
 	{
 		get
 		{
 			if (instance == null)
 			{
-				Type type = typeof(T);
-				GameObject[] objs = GameObject.FindGameObjectsWithTag(MANAGER_TAG);
-				for (int j = 0; j < objs.Length; j++)
+				instance = (T)FindObjectOfType(typeof(T));
+				if (instance == null)
 				{
-					instance = (T)objs[j].GetComponent(type);
-					if (instance != null)
-						return instance;
+					Debug.LogError(typeof(T) + "is nothing");
 				}
-
-				Debug.LogWarning(string.Format("{0} がありません", type.Name));
 			}
 			return instance;
 		}
 	}
 
-	/// <summary>
-	/// 開始処理
-	/// </summary>
-	virtual protected void Awake()
+	protected virtual void Awake()
 	{
 		CheckInstance();
 	}
-
-	/// <summary>
-	/// インスタンスがあるかをチェックする
-	/// </summary>
-	/// <returns></returns>
+	
 	protected bool CheckInstance()
 	{
-		if (instance == null)
-		{
-			instance = (T)this;
-			return true;
-		}
-		else if (Instance == this)
-		{
-			return true;
-		}
-
+		if (this == Instance) { return true; }
 		Destroy(this);
 		return false;
 	}
