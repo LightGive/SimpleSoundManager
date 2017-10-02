1c7796e47a998522646ec92232ce9c3295089027
VIVE_PC_3 <roi.akase@outlook.jp>
2017-10-02 22:08:42 +0900

    シーン修正、AudioManagerのプレハブ作成

---
+++ SimpleAudioManager/Assets/SimpleAudioManager/Example/Scripts/ExampleScene1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ExampleScene1
+/// </summary>
+public class ExampleScene1 : MonoBehaviour {
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+	public void Play()
+	{
+
+	}
+}
--- SimpleAudioManager/Assets/SimpleAudioManager/Scripts/AudioName.cs
+++ SimpleAudioManager/Assets/SimpleAudioManager/Scripts/AudioName.cs
@@ -1,5 +1,6 @@
 ﻿public static class AudioName
 {
+	public const string BGM_Example2 = "Example2";
 	public const string BGM_Main = "Main";
 	
 	public const string SE_Computer = "Computer";
