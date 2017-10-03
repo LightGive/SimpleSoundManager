3113b5b7ba2c18799b9e8940a3d2647626fe8e4c
VIVE_PC_3 <roi.akase@outlook.jp>
2017-10-03 09:13:44 +0900

    シーン修正

--- SimpleAudioManager/Assets/SimpleAudioManager/Example/Scripts/ExampleScene1.cs
+++ SimpleAudioManager/Assets/SimpleAudioManager/Example/Scripts/ExampleScene1.cs
@@ -1,24 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// ExampleScene1
 /// </summary>
 public class ExampleScene1 : MonoBehaviour {
 
-	void Start ()
+	[SerializeField]
+	private Text volumeText;
+	[SerializeField]
+	private Text delayText;
+	[SerializeField]
+	private Text pitchText;
+
+	private float volume = 1.0f;
+	private float delay = 0.0f;
+	private float pitch = 1.0f;
+
+	void Update()
 	{
-		
-	}
-	
-	void Update ()
-	{
-		
+
 	}
 
-	public void Play()
+	public void PlaySound2D()
 	{
+		AudioManager.Instance.PlaySound2D(AudioName.SE_Shutter, volume, delay, pitch);
+	}
 
+	public void PlaySound2DLoop()
+	{
+		AudioManager.Instance.PlaySound2DLoop(AudioName.SE_Shutter, 5, volume, delay, pitch);
+	}
+
+	public void PlayBGM()
+	{
+		AudioManager.Instance.PlayBGM(AudioName.BGM_Main);
+	}
+
+	public void ChangeVolume(float sliderValue)
+	{
+		volume = sliderValue;
+		volumeText.text = (sliderValue * 100).ToString("f1") + "%";
+	}
+
+	public void ChangeDelay(float sliderValue)
+	{
+		delay = sliderValue;
+		delayText.text = sliderValue.ToString("f2") + " sec";
+	}
+
+	public void ChangePitch(float sliderValue)
+	{
+		pitch = sliderValue;
+		pitchText.text = sliderValue.ToString("f2");
 	}
 }
