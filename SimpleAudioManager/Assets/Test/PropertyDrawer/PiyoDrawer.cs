6e1fa219c6b3cc1ffcf19bc74c039453dae73bca
VIVE_PC_3 <roi.akase@outlook.jp>
2017-10-11 16:11:44 +0900

    PropertyDrawerを試しつつ、不具合を修正

---
+++ SimpleAudioManager/Assets/Test/PropertyDrawer/PiyoDrawer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PiyoDrawer : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		AudioClipInfoAttribute att = (AudioClipInfoAttribute)attribute;
+		using (new EditorGUI.PropertyScope(position, label, property))
+		{
+		}
+	}
+}
