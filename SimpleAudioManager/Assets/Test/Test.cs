d409011c65d4bdd69e85bad70183531b0aa24f90
VIVE_PC_3 <roi.akase@outlook.jp>
2017-10-11 21:29:33 +0900

    テストコードを削除

--- SimpleAudioManager/Assets/Test/Test.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Test : MonoBehaviour
-{
-	[SerializeField, Piyo]
-	private List<Piyo> piyo = new List<Piyo>();
-}
