b8bd55a61dd1935be2bd0d2f6a3aaeb9e2e1b383
VIVE_PC_3 <roi.akase@outlook.jp>
2017-11-04 16:29:45 +0900

    Sliderに簡単に音を追加できるように変更中

--- SimpleAudioManager/Assets/SimpleSoundManager/Scripts/Utility/UISliderSoundSetting.cs
+++ SimpleAudioManager/Assets/SimpleSoundManager/Scripts/Utility/UISliderSoundSetting.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UISliderSoundSetting : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+[RequireComponent(typeof(Slider))]
+public class UISliderSoundSetting : MonoBehaviour
+{
+	[SerializeField]
+	private float changeValue;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	private int preValue;
+	private int splitCount;
+	private float offset = 0.0f;
+
+	private Slider slider;
+
+	void Start()
+	{
+		slider = this.gameObject.GetComponent<Slider>();
+		slider.onValueChanged.AddListener(ValueChange);
+		offset = -slider.minValue;
+		splitCount = Mathf.FloorToInt((slider.maxValue + offset) / changeValue);
+
+		offset = (slider.maxValue - slider.minValue);
+		if (changeValue > offset)
+		{
+			changeValue = offset;
+		}
+	}
+
+	void Update()
+	{
+
+	}
+
+	void ValueChange(float _value)
+	{
+		int index = Mathf.FloorToInt((_value + offset) / changeValue);
+		if(index != preValue)
+		{
+			SimpleSoundManager.Instance.PlaySound2D(AudioNameSE.Bound);
+			preValue = index;
+		}
+
 	}
 }
