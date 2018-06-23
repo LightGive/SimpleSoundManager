cdc9d9ec6de7c677a370a47e0e050532f677aade
LightGive <roi.akase@outlook.jp>
2018-06-23 12:00:17 +0900

    ファイルパスなど

---
+++ SimpleSoundManager/Assets/LightGive/SimpleSoundManager/Scripts/SimpleSoundManagerDefine.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SimpleSoundManagerDefine
+{
+	public static string PathBgmSource
+	{
+		get { return Application.dataPath + ""; }
+	}
+}
