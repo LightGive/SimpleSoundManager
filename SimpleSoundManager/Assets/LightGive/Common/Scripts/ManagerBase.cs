46a27b3a059edcdaf4b6f97b8cad2da3d8e3ae61
LightGive <roi.akase@outlook.jp>
2018-06-23 11:22:45 +0900

    改めてManagerを作成

--- SimpleSoundManager/Assets/LightGive/Common/Scripts/ManagerBase.cs
+++ SimpleSoundManager/Assets/LightGive/Common/Scripts/ManagerBase.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ManagerBase : SingletonMonoBehaviour<ManagerBase>
+public class ManagerBase : MonoBehaviour
 {
-	protected override void Awake()
+	private void Awake()
 	{
-		base.Awake();
+		
 	}
 }
---
+++ SimpleSoundManager/Assets/LightGive/SimpleSoundManager/Scripts/SimpleSoundManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleSoundManager : SingletonMonoBehaviour<SimpleSoundManager>
+{
+	protected override void Awake()
+	{
+		base.Awake();
+	}
+}
