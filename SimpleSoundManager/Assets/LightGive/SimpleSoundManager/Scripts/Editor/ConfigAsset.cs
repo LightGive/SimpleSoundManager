759d15982a2b149f8b3c2a3227d2aa471d72341a
LightGive <roi.akase@outlook.jp>
2018-06-23 12:50:28 +0900

    ScriptableObjectを設定データとして作成するところの処理

---
+++ SimpleSoundManager/Assets/LightGive/SimpleSoundManager/Scripts/Editor/ConfigAsset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ConfigAsset : ScriptableObject
+{
+	static public void CreateAsset()
+	{
+		var configAsset = CreateInstance<ConfigAsset>();
+		AssetDatabase.CreateAsset(configAsset, SimpleSoundManagerDefine.PathConfigData);
+		AssetDatabase.Refresh();
+	}
+}
--- SimpleSoundManager/Assets/LightGive/SimpleSoundManager/Scripts/Editor/SimpleSoundManagerSetting.cs
+++ SimpleSoundManager/Assets/LightGive/SimpleSoundManager/Scripts/Editor/SimpleSoundManagerSetting.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public static class SimpleSoundManagerSetting
 {
 	public static void CreateConfigData()
 	{
-
+		ConfigAsset.CreateAsset();
 	}
 }
