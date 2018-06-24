55e005a026d49ef8b47e6b28f72c47d02ec4e983
LightGive <roi.akase@outlook.jp>
2018-06-24 10:28:42 +0900

    シーン修正

--- SimpleSoundManager/Assets/LightGive/SimpleSoundManager/Scripts/SimpleSoundManager.cs
+++ SimpleSoundManager/Assets/LightGive/SimpleSoundManager/Scripts/SimpleSoundManager.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class SimpleSoundManager : SingletonMonoBehaviour<SimpleSoundManager>
 {
+	[SerializeField]
+	private List<SoundEffectPlayer> m_soundEffectPlayers = new List<SoundEffectPlayer>();
+	[SerializeField]
+	private int m_sePlayerNum = 10;
+
 	protected override void Init()
 	{
 		base.Init();
+		for (int i = 0; i < m_sePlayerNum;i++)
+		{
+			GameObject soundPlayerObj = new GameObject("SoundPlayer" + i.ToString("0"));
+			soundPlayerObj.transform.SetParent(transform);
+			SoundEffectPlayer player = soundPlayerObj.AddComponent<SoundEffectPlayer>();
+			m_soundEffectPlayers.Add(player);
+		}
 	}
 }
---
+++ SimpleSoundManager/Assets/LightGive/SimpleSoundManager/Scripts/SoundEffectPlayer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectPlayer : MonoBehaviour
+{
+	private AudioClip m_audioClip;
+}
