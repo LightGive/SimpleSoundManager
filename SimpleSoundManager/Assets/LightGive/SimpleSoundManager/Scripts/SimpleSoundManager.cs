e4fb5930db8cafb57648e0402e325f0fd7235393
LightGive <roi.akase@outlook.jp>
2018-06-27 23:34:56 +0900

    修正

--- SimpleSoundManager/Assets/LightGive/SimpleSoundManager/Scripts/SimpleSoundManager.cs
+++ SimpleSoundManager/Assets/LightGive/SimpleSoundManager/Scripts/SimpleSoundManager.cs
@@ -169,6 +169,7 @@ public class SimpleSoundManager : SingletonMonoBehaviour<SimpleSoundManager>
 			player.animationCurve = animCurve;
 		}
 
+
 		if (_is3dSound)
 		{
 			player.source.minDistance = _minDistance;
