b6ac1b0973267189b28087d732ea2cdfde2bcc18
LightGive <roi.akase@outlook.jp>
2018-10-22 11:30:48 +0900

    Example削除

--- SimpleSoundManager/Assets/LightGive/Managers/SimpleSoundManager/Scripts/SoundName.cs
+++ SimpleSoundManager/Assets/LightGive/Managers/SimpleSoundManager/Scripts/SoundName.cs
@@ -1,28 +1,14 @@
 ﻿public static class SoundName
 {
-	public const string BGM_Example1 = "Example1";
-	public const string BGM_Example2 = "Example2";
 	
-	public const string SE_BoundBall = "BoundBall";
-	public const string SE_ButtonDown = "ButtonDown";
-	public const string SE_Cursor = "Cursor";
-	public const string SE_Helicopter = "Helicopter";
-	public const string SE_typewriter = "typewriter";
 }
 	
 public enum SoundNameBGM
 {
 	None,
-	Example1 = 1,
-	Example2 = 2,
 }
 	
 public enum SoundNameSE
 {
 	None,
-	BoundBall = 1,
-	ButtonDown = 2,
-	Cursor = 3,
-	Helicopter = 4,
-	typewriter = 5,
 }
